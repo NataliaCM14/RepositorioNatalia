3d8c0169044b77691a1a7e88be4872e8dec2ec4e
NataliaCM14 <168367572+NataliaCM14@users.noreply.github.com>
2024-10-03 23:23:19 +0200

    Add files via upload

---
+++ Alumnos_y_notas_Objetos/src/notas/Alumno.java
@@ -0,0 +1,88 @@
+package notas;
+
+import java.util.Arrays;
+
+public class Alumno {
+
+    //ATRIBUTOS:
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private int[] notas;
+
+
+
+
+    //CONSTRUCTORES:
+
+    public Alumno() {
+
+    }
+    public Alumno(String nombre, String apellidos, int edad, int[] notas) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.notas = notas;
+    }
+
+
+    //GETTERS:
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int[] getNotas() {
+        return notas;
+    }
+
+
+
+    //SETTERS:
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNotas(int[] notas) {
+        this.notas = notas;
+    }
+
+
+
+    //MÉTODOS:
+    public void getNotaMedia() {
+        int sum = 0;
+        int nota_media;
+
+        for (int nota : notas) {
+            sum += nota;
+        }
+
+        nota_media = sum/notas.length;
+
+        System.out.println("\nLa nota media del alumno es: " + nota_media);
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\n" +
+                "Apellidos: " + apellidos + "\n" +
+                "Edad: " + edad + "\n" +
+                "Notas: " + Arrays.toString(notas) + "\n";
+    }
+}
